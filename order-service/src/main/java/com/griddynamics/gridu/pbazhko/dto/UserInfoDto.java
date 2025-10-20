4fc0ce2d8b5808edf25cfacd6a50f3a9da6a69f8
Pavel Bazhko <pbazhko@griddynamics.com>
2025-10-20 15:02:13 +0200

    Implement user info functionality

--- order-service/src/main/java/com/griddynamics/gridu/pbazhko/dto/UserInfoDto.java
+++ order-service/src/main/java/com/griddynamics/gridu/pbazhko/dto/UserInfoDto.java
@@ -1,12 +1,8 @@
 package com.griddynamics.gridu.pbazhko.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserInfoDto {
     private String id;
     private String name;
